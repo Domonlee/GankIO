6a8482d2d8b6328c1d7804efd63fa65a9cf66409
Domon <viplizhao@gmail.com>
2016-08-09 18:34:31 +0800

    添加抽屉头部

--- app/src/main/java/domon/cn/gankio/MainActivity.java
+++ app/src/main/java/domon/cn/gankio/MainActivity.java
@@ -7,12 +7,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Toast;
 
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -41,24 +45,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setUpDrawer() {
-        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1)
-                .withName("home");
-        PrimaryDrawerItem item2 = new SecondaryDrawerItem().withIdentifier(2)
+
+        ProfileDrawerItem profileDrawerItem = new ProfileDrawerItem().withName("点击登录");
+
+        PrimaryDrawerItem itemHome = new PrimaryDrawerItem()
+                .withIcon(R.mipmap.ic_launcher)
+                .withName("Home");
+        PrimaryDrawerItem item2 = new PrimaryDrawerItem()
+                .withIcon(R.mipmap.ic_launcher)
                 .withName("second");
 
-        Drawer result = new DrawerBuilder()
+        AccountHeader accountHeader = new AccountHeaderBuilder()
                 .withActivity(this)
+                .withHeaderBackground(R.mipmap.ic_launcher)
+                .addProfiles(profileDrawerItem)
+                .withOnAccountHeaderItemLongClickListener(new AccountHeader.OnAccountHeaderItemLongClickListener() {
+                    @Override
+                    public boolean onProfileLongClick(View view, IProfile profile, boolean current) {
+                        return false;
+                    }
+                }).build();
+
+        mDrawer = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(mToolbar)
+                .withAccountHeader(accountHeader)
                 .addDrawerItems(
-                        item1,
+                        itemHome,
                         new DividerDrawerItem(),
                         item2,
                         new SecondaryDrawerItem().withName("hello")
+                                .withIcon(R.mipmap.ic_launcher)
                 )
                 .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
                     @Override
                     public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
                         Toast.makeText(MainActivity.this, position + "", Toast.LENGTH_LONG).show();
-                        return false;
+                        return true;
                     }
                 }).build();
     }
