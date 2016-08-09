c34b23a2ce3926fef6dfa683a99770b6d5c7a35d
Domon <viplizhao@gmail.com>
2016-08-09 17:31:03 +0800

    实现toolbar+drawer抽屉效果

--- app/src/main/java/domon/cn/gankio/MainActivity.java
+++ app/src/main/java/domon/cn/gankio/MainActivity.java
@@ -1,13 +1,71 @@
 package domon.cn.gankio;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
+    private Drawer mDrawer;
+
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mContext = this;
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
+
+        setUpDrawer();
+
+        getSupportActionBar().setTitle("Hello World");
+
+    }
+
+    private void setUpDrawer() {
+        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1)
+                .withName("home");
+        PrimaryDrawerItem item2 = new SecondaryDrawerItem().withIdentifier(2)
+                .withName("second");
+
+        Drawer result = new DrawerBuilder()
+                .withActivity(this)
+                .addDrawerItems(
+                        item1,
+                        new DividerDrawerItem(),
+                        item2,
+                        new SecondaryDrawerItem().withName("hello")
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        Toast.makeText(MainActivity.this, position + "", Toast.LENGTH_LONG).show();
+                        return false;
+                    }
+                }).build();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ButterKnife.unbind(this);
     }
 }
